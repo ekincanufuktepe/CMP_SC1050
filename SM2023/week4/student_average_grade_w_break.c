1e09159d1d93e68ca8e6eea04c8883cccaa073d7
Ekincan Ufuktepe <ekincanufuktepe@gmail.com>
2023-07-05 09:18:15 -0500

    A simple break usage example
    This is similar to what we did with a sentinal variable
    controlled loop. Instead of using a sentinel variable,
    we used it with break.

---
+++ SM2023/week4/student_average_grade_w_break.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int main() {
+
+    int grade;
+    int counter = 0;
+    int total;
+
+    float average;
+
+    total = 0;
+    while(1) {
+        printf("Enter grade, enter -1 to stop\n");
+        scanf("%d", &grade);
+        if(grade != -1) {
+            //total = total + grade;
+            total += grade;
+            //counter = counter + 1;
+            counter += 1;
+        }
+        else {
+            break;
+            printf("Break!");
+        }
+        printf("While loop region\n");
+    }
+
+    if(counter == 0) {
+        printf("You have no students\n");
+    }
+    else {
+        average = (float)total/counter;
+        printf("Number of students: %d\n", counter);
+        printf("Average of grades: %f\n", average);
+    }
+
+    return 0;
+}
