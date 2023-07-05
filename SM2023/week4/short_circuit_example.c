53dcca145aab432f0ee7fd802aa326be47d6be47
Ekincan Ufuktepe <ekincanufuktepe@gmail.com>
2023-07-05 09:26:28 -0500

    Short circuit example added
    While using logical operators, depending on the logical
    operator, the next expression may not be executed. For instance,
    in an if-statement where there are two expression given as
    condition connected with an AND operator, if the first expression
    is false, the second expression does not get executed This is
    because of the AND logic operator. If at leastone of the
    expression is FALSE, no matter what the other expressions are,
    the result will be FALSE, this is an example of short circuit.
    Try executing the code, and see if the second expression will
    be executed by checking the print result.

---
+++ SM2023/week4/short_circuit_example.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+
+    int x = 0;
+    int y = 0;
+
+    if(y == 1 && ++x == 1) {
+        //printf("x:%d, y:%d\n", x, y);
+    }
+    printf("x:%d, y:%d\n", x, y);
+    return 0;
+}
