8a37d1e6a805c1953ef3985e7f30f793542cbf20
Ekincan Ufuktepe <ekincanufuktepe@gmail.com>
2023-07-10 09:18:25 -0500

    Call by reference example
    aka. pass-by-reference.

---
+++ SM2023/week5/call_by_reference_example.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int update(int*);
+
+int main() {
+    int x = 2;
+    printf("Before update: %d\n", x);
+    printf("Update function result: %d\n", update(&x));
+    printf("After update: %d\n", x);
+    return 0;
+}
+
+int update(int *x) {
+    *x = *x * 5;
+    return *x;
+}
