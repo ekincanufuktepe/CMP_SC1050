ef7a4b5d1f301614b1f7e8aad2b12a08922cc24b
ekincanufuktepe <ekincanufuktepe@gmail.com>
2023-07-18 09:50:08 -0500

    A simple const usage example

---
+++ SM2023/week7/const_example_1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+
+    const int x = 0 + 1;
+    int y = x + 7; // you can read const variables
+    //x = 5; // const will not allow you to modify/write the variables
+    return 0;
+}
