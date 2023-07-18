8990a774a677e7f9db28bde4aa1baa2ed3f1f802
ekincanufuktepe <ekincanufuktepe@gmail.com>
2023-07-18 15:54:53 -0500

    Bug fix for compile error in function print_student_grades()
    The bug was not having consistent size values given for the
    student name, which the size of exams numbers, not max number
    of characters. We also refactored the defines to meaningful
    names that we could easily relate.

--- SM2023/week7/student_data.c
+++ SM2023/week7/student_data.c
@@ -2,53 +2,59 @@
 #include<time.h>
 #include<stdlib.h>
 
-#define ROWS 3
-#define COLUMNS 4
+#define NUM_OF_STUDENTS 3
+#define NUM_OF_EXAMS 4
+#define MAX_CHARS_NAME 50
 
-void print_student_grades(int[][COLUMNS], char[][COLUMNS]);
+void print_student_grades(int[][NUM_OF_EXAMS], char[][MAX_CHARS_NAME]);
+float average_of_exams(int exam_grades[]);
 
 int main() {
     srand(time(NULL));
 
-    char student_name[ROWS][50];
-    int student_grades[ROWS][COLUMNS] = {};
+    char student_name[NUM_OF_STUDENTS][MAX_CHARS_NAME];
+    int student_grades[NUM_OF_STUDENTS][NUM_OF_EXAMS] = {};
 
-    for(int i=0; i<ROWS; i++) {
+    for(int i=0; i<NUM_OF_STUDENTS; i++) {
         scanf("%s", student_name[i]);
     }
 
-    for(int i=0; i<ROWS; i++ ) {
-        for(int j=0; j<COLUMNS; j++) {
+    for(int i=0; i<NUM_OF_STUDENTS; i++ ) {
+        for(int j=0; j<NUM_OF_EXAMS; j++) {
             student_grades[i][j] = rand()%40 + 60;
         }
     }
 
-    for(int i=0; i<ROWS; i++) { // access each student
-        printf("%s: ", student_name[i]);
-        for(int j=0; j<COLUMNS; j++) { // access grades
-            printf("%d ", student_grades[i][j]);
-        }
-        printf("\n");
-    }
+    // for(int i=0; i<ROWS; i++) { // access each student
+    //     printf("%s: ", student_name[i]);
+    //     for(int j=0; j<NUM_OF_EXAMS; j++) { // access grades
+    //         printf("%d ", student_grades[i][j]);
+    //     }
+    //     printf("\n");
+    // }
 
     print_student_grades(student_grades, student_name);
 
     return 0;
 }
 
-void print_student_grades(int student_grades[][COLUMNS], char student_name[][COLUMNS]) {
-    for(int i=0; i<ROWS; i++ ) { // access each student
+
+// The second dimension of the student name argument/parameter was inconsistent
+void print_student_grades(int student_grades[][NUM_OF_EXAMS], char student_name[][MAX_CHARS_NAME]) {
+    for(int i=0; i<NUM_OF_STUDENTS; i++ ) { // access each student
         printf("%s: ", student_name[i]);
-        for(int j=0; j<COLUMNS; j++) { // access grades
+        for(int j=0; j<NUM_OF_EXAMS; j++) { // access grades
             printf("%d ", student_grades[i][j]);
         }
+        printf(", Avg: %f", average_of_exams(student_grades[i]));
         printf("\n");
     }
 }
 
-//    for(int i=0; i<ROWS; i++ ) {
-//         for(int j=0; j<COLUMNS; j++) {
-//             printf("%d ", a[i][j]);
-//         }
-//         printf("\n");
-//     }
+float average_of_exams(int exam_grades[]) {
+    float sum = 0;
+    for(int i=0; i<NUM_OF_EXAMS; i++) {
+        sum = sum + exam_grades[i];
+    }
+    return sum/NUM_OF_EXAMS;
+}
