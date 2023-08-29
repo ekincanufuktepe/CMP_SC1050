d5517318067b30238dbfeee7871243cb87959732
ekincanufuktepe <ekincanufuktepe@gmail.com>
2023-08-29 14:33:08 -0500

    The good old Hello world example...

---
+++ FS2023/week1/whatsup.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// To compile this code use the command below:
+// gcc whatsup.c
+// this will generate an executable file named "a.out"
+// to execute the file "a.out", write: ./a.out on the terminal
+int main() {
+    printf("Welcome to CS1050!!\n");
+    return 0;
+}
