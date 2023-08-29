677b93b778e8ce592394383c2730d8d61cacb949
ekincanufuktepe <ekincanufuktepe@gmail.com>
2023-08-29 14:30:27 -0500

    Adding a simple code for basic input/output
    The code gets the birth year of the user and prints the birth
    year back. A simple example and application of printf and scanf.

---
+++ FS2023/week2/calculate_age.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+
+    int birth_year;
+    printf("Enter your birth year: ");
+    scanf("%d", &birth_year);
+    printf("You were born in %d\n", birth_year);
+
+    return 0;
+}
