a93aa45b54a60d8c36c126c9852e5a2a333525c9
ekincanufuktepe <ekincanufuktepe@gmail.com>
2023-09-21 19:03:05 -0500

    A very basic and simple for statement example
    The for loop simply iterates 10 times in this example and prints
    the numbers.

---
+++ FS2023/week5/for_statement_ex.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+
+    int counter;
+
+    for(counter = 0; counter < 10; counter++) {
+        printf("%d\n", counter);
+    }
+    //printf("%d\n", counter);
+
+    return 0;
+}
