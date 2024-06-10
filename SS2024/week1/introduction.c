4541fa4401263c8934ebd448a72e6a789adcc723
ekincanufuktepe <ekincanufuktepe@gmail.com>
2024-06-10 10:09:46 -0500

    Introduction to programming
    Example of line comments, and block comments given.
    A very simple printing example provided.

---
+++ SS2024/week1/introduction.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+// this is the main of the code
+// this will start your code
+
+/* This is a block comment example
+   see this is still commented*/
+void main() {
+    printf("Welcome to Mizzou EECS!\n");
+}
