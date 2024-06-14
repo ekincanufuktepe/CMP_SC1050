2962094f5b2561774a88330c6963f757a5dc11ec
ekincanufuktepe <ekincanufuktepe@gmail.com>
2024-06-14 10:26:20 -0500

    Keywords/Reserved words cannot be used for variable names

---
+++ SS2024/week2/keywords.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    // int 1int = 0; // this is an invalid variable name, they cannot start with a number
+    // printf("%d\n", 1int); 
+    int grade = 7;
+    // use " for strings
+    // use ' for characters (char)
+
+    printf("Status: %d\n", grade >= 60 ? 1 : 0);
+
+    return 0;
+}
