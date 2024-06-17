81f179c16f0d810bfb051b0a9a8667d36d54357d
ekincanufuktepe <ekincanufuktepe@gmail.com>
2024-06-17 13:21:42 -0500

    Infinite loop example

---
+++ SS2024/week2/inifinte_loop.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+
+    // infinite loop example
+    while(-1) { // non-zero values are true, and the value zero is false.
+        printf("I will run forever!!!\n");
+    }
+
+    return 0;
+}
