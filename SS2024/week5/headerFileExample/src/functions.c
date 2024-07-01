68d0aec886398068f517d32ae3cac597652c207f
ekincanufuktepe <ekincanufuktepe@gmail.com>
2024-07-01 12:59:29 -0500

    Functions with header files

---
+++ SS2024/week5/headerFileExample/src/functions.c
@@ -0,0 +1,40 @@
+#include "../header/functions.h"
+
+int factorial(int n) {
+    int result = 1;
+    if(n == 0) {
+        return 1;
+    }
+    for(int i=1; i<=n; i++) {
+        result = result * i;
+    }
+    return result;
+}
+
+float fact(float n) {
+    float result = 1;
+    if(n == 0) {
+        return result;
+    }
+    for(int i=1; i<=n; i++) {
+        result = result * i;
+    }
+    return result;
+}
+
+void printFibonnacciSequence(int sizeOfSequence) {
+    int initialValue = 1;
+    int previousValue = 0;
+    for(int i=0; i<sizeOfSequence; i++) {
+        if(i == 0 || 1 == i) {
+            printf("%d ", initialValue);
+            previousValue = initialValue;
+        }
+        else {
+            int tmp = initialValue;
+            initialValue = previousValue + initialValue;
+            previousValue = tmp;
+            printf("%d ", initialValue);
+        }
+    }
+}
---
+++ SS2024/week5/headerFileExample/src/main.c
@@ -0,0 +1,7 @@
+#include "../header/functions.h"
+
+int main() {
+    printf("Result: %d\n", factorial(5));
+    printFibonnacciSequence(13);
+    return 0;
+}
