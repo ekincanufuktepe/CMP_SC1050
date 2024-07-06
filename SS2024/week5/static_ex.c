c3b5738c5e0a453e6fec51e8f536ec5ba5b407e4
ekincanufuktepe <ekincanufuktepe@gmail.com>
2024-07-06 13:16:31 -0500

    A local static variable example in a function

--- SS2024/week5/static_ex.c
+++ SS2024/week5/static_ex.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
 void func();
 
-for(int x=0, char z='a')
-
 int main() {
     func();
     func();
@@ -12,5 +10,4 @@ int main() {
 
 void func() {
     static int x = 0; // static variables are similar global variables
-    printf("x: %d\n", x++);
 }
