26dc7d5535752a0fae6c977816bb07930f592e89
ekincanufuktepe <ekincanufuktepe@gmail.com>
2024-09-04 10:02:30 -0500

    First hello world C code.

---
+++ FS2024/week1/hello.c
@@ -0,0 +1,11 @@
+#include<stdio.h> // LINE COMMENT: this is a directive C preprocessor
+
+/*  BLOCK COMMENT
+    This is our first program 
+    It will print "Welcome to 
+    CS1050" on the terminal */
+
+int main() {
+    printf("Welcome to CS1050\\CS1050H\n\"Algorithm Design and Programming I\"\n");
+    return 0;
+}
