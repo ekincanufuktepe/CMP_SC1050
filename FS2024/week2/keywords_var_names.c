4f0c4bf9fc4a52a22b5e2e83b7dc24be2e7b6281
ekincanufuktepe <ekincanufuktepe@gmail.com>
2024-09-04 10:05:08 -0500

    Variable naming rules

---
+++ FS2024/week2/keywords_var_names.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+
+    // You cannot use keywords for variable and function names
+    int if = 5; // bad
+    int int = 2; // bad
+    // Also, your variable names cannot start with a number
+    int 1_variable = 1; // bad
+
+    return 0;
+}
