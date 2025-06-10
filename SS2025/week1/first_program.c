c1bdc5676e7b4eacdc17199e7e7b954b6e5909a8
ekincanufuktepe <ekincanufuktepe@gmail.com>
2025-06-09 21:18:47 -0500

    First C program added

---
+++ SS2025/week1/first_program.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/* 
+    This is a "block comment"
+    I can write as long as I want 
+    without worrying of the number of lines
+*/
+int main() {
+    // this is a "line comment" and you can only have the comment for one line
+    // if I want to continue with the comment you need another double forward slash
+    printf("Hello, I am Ekin!\nNice to meet you all\n\tI hope you are enjoying this\n");
+    return 0;
+}
