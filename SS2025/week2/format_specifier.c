0b86339a1abf507b2086c8bca10ed7a72ec48afc
ekincanufuktepe <ekincanufuktepe@gmail.com>
2025-06-20 21:40:09 -0500

    format specifier usage example added

---
+++ SS2025/week2/format_specifier.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+
+    printf("%s %s\n", "Ekincan", "Ufuktepe");
+    double result = pow(2,0);
+    printf("Result: %lf\n", result);
+
+    return 0;
+}
