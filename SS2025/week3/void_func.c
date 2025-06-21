908a95f987583463e01f5fcc3ec2f34aa7486377
ekincanufuktepe <ekincanufuktepe@gmail.com>
2025-06-20 21:44:32 -0500

    void function example added

---
+++ SS2025/week3/void_func.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void greetUser();
+
+int main() {
+    greetUser();
+    return 0;
+}
+
+
+void greetUser() {
+    printf("Hello User!\n");
+    return;
+    printf("How are you today?\n");
+}
