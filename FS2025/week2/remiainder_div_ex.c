97d5c3300a4e1f0393bd382db599689171779ed1
ekincanufuktepe <ekincanufuktepe@gmail.com>
2025-09-09 11:20:48 -0500

    remainder (mod) example added

---
+++ FS2025/week2/remiainder_div_ex.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+
+    int a = 8;
+    int b = 3;
+
+    // int a = -8;
+    // int b = -3;
+
+    printf("(int division) Result: %d\n", a/b);
+    printf("(remainder) Result: %d\n", a%b);
+
+    return 0;
+}
