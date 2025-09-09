4f8c0febc99489805d1fe06d6e07fa600e97c79f
ekincanufuktepe <ekincanufuktepe@gmail.com>
2025-09-09 11:18:08 -0500

    First hello world example with comments

---
+++ FS2025/week2/hello.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+// Line comment, will only comment one line
+
+/* The beginning of the block comment
+    This is a block comment
+
+    End of the block comment */
+
+int main() {
+    // this is where we print Hello World
+    printf("\\Hello \"World!!!\n");
+
+    return 0;
+}
