62ed2ebe7b897cde4d2a1e22461617a475ca5fe7
ekincanufuktepe <ekincanufuktepe@gmail.com>
2025-09-18 11:11:47 -0500

    for-statement with comma and semi-colon

---
+++ FS2025/week4/for_ex2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+
+    ;
+    int sum = 0;
+    // Test the difference between for() and for();
+    for(int num=2; num <= 100; sum+=num, num+=2);
+    
+    printf(" ");
+        
+    printf("sum: %d\n", sum);
+    return 0;
+}
