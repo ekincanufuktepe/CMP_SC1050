e52c970f5973d9b4d70230d9591f7996f60f18c3
ekincanufuktepe <ekincanufuktepe@gmail.com>
2025-09-25 11:49:04 -0500

    do-while example added

---
+++ FS2025/week5/do_while.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+
+    int x = 99;
+    // do{
+    //     printf("x: %d\n", x);
+    // }while(++x <= 10);
+
+    while(++x <= 10){
+        printf("x: %d\n", x);
+    }
+
+    return 0;
+}
