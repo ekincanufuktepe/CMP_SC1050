318bf9514a5540572dfd166f5b480bd817462078
ekincanufuktepe <ekincanufuktepe@gmail.com>
2025-10-24 14:31:58 -0500

    char arrays added

---
+++ FS2025/week9/char_arrays.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+
+    // char name[5] = {'e', 'k', 'i', 'n'};
+    char name[5];
+
+    scanf("%3s", name);
+
+    printf("Name: %s\n", name);
+
+    return 0;
+}
